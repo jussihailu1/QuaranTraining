fd5e9d06cbffa44682df1925a45dc5cfd62dacd7
jussi <45388631+jussihailu1@users.noreply.github.com>
2020-04-22 21:04:51 +0200

    Folder layout

--- Quarantraining/client/script.js
+++ Quarantraining/client/script.js
@@ -97,3 +97,28 @@ function initDifficulty() {
 }
 
 initDifficulty();
+
+function loadChallenges(){
+    const challengeContainer = document.getElementById()
+}
+
+
+
+
+// Functionality
+function getChallenges() {
+    fetch("http://127.0.0.1:3000/challenges")
+        .then(response => response.json())
+        .then(challenges => {
+            console.log(challenges);
+            return challenges;
+        });
+}
+
+function postChallenge() {
+
+}
+
+function getSharedChallenges() {
+
+}
