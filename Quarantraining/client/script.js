77dee5bf5f4d31cb2f25e70dcb8a1ddf985a2232
jussi <45388631+jussihailu1@users.noreply.github.com>
2020-04-22 21:21:59 +0200

    Can now post and get challenges and shared challenges

--- Quarantraining/client/script.js
+++ Quarantraining/client/script.js
@@ -97,28 +97,3 @@ function initDifficulty() {
 }
 
 initDifficulty();
-
-function loadChallenges(){
-    const challengeContainer = document.getElementById()
-}
-
-
-
-
-// Functionality
-function getChallenges() {
-    fetch("http://127.0.0.1:3000/challenges")
-        .then(response => response.json())
-        .then(challenges => {
-            console.log(challenges);
-            return challenges;
-        });
-}
-
-function postChallenge() {
-
-}
-
-function getSharedChallenges() {
-
-}
