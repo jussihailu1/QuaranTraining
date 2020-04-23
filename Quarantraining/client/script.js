fca0fae24c0baf5af73d4ac98c4dc6a712aea7d3
jussi <45388631+jussihailu1@users.noreply.github.com>
2020-04-23 10:45:47 +0200

    Everything works accept PWA stuff

--- Quarantraining/client/script.js
+++ Quarantraining/client/script.js
@@ -1,7 +1,7 @@
 //See if the browser supports Service Workers, if so try to register one
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register("service-worker.js")
+    .register("client\service-worker.js")
     .then(function (registering) {
       // Registration was successful
       console.log(
@@ -32,10 +32,10 @@ const pages = document.getElementsByClassName("page");
 for (const button of navButtons) {
   button.onclick = function () {
     for (const btn of navButtons) {
-      btn.firstChild.classList.remove("active");
+      btn.children[0].classList.remove("active");
     }
     const selectedPage = button.id.split("-")[1];
-    button.firstChild.classList.add("active");
+    button.children[0].classList.add("active");
     navigatePage(selectedPage);
   };
 }
@@ -59,7 +59,7 @@ const difficulty = {
     sticky: false,
   },
   title: document.getElementById("tab-title"),
-  levels: ["easy", "medium", "hard"],
+  levels: ["easy", "medium", "hard", "extreme"],
   current: {
     name: "",
     getIndex: function () {
@@ -122,7 +122,7 @@ document.onscroll = function () {
 };
 
 function initDifficulty() {
-  difficulty.controls.naturalPosition = difficulty.controls.container.offsetTop;
+  difficulty.controls.naturalPosition = difficulty.controls.container.offsetTop + 1;
   difficulty.current.name = difficulty.levels[0];
 
   toggleControls();
@@ -137,3 +137,62 @@ for (const vid of document.getElementsByClassName("post-vid")) {
     vid.load();
   });
 }
+
+let dailyChallenges;
+async function init() {
+  await fetch("http://127.0.0.1:3000/challenges").then(response => response.json()).then(challenges => { dailyChallenges = challenges; });
+  loadDailyChallenges();
+}
+
+function loadDailyChallenges() {
+  const dcEasyContainer = document.getElementById('daily-challenges-easy');
+  const dcMediumContainer = document.getElementById('daily-challenges-medium');
+  const dcHardContainer = document.getElementById('daily-challenges-hard');
+
+  const easyChallenges = dailyChallenges.filter(dc => dc.level == "easy");
+  const mediumChallenges = dailyChallenges.filter(dc => dc.level == "medium");
+  const hardChallenges = dailyChallenges.filter(dc => dc.level == "hard");
+
+  let dcEasyString = "";
+  let dcMediumString = "";
+  let dcHardString = "";
+
+  function cardString(name, image) {
+    return `<div class="card">
+              <img src="${image}" alt="${name}" class="challenge-img" />
+              <div class="card-body">
+                <h5 class="challenge-name">${name.toUpperCase()}</h5>
+              </div>
+            </div>`;
+  }
+
+  for (let i = 0; i < easyChallenges.length; i += 2) {
+    const dcs = [easyChallenges[i], easyChallenges[i + 1]]
+    let dcString = '<div class="row">';
+    for (const dc of dcs) { dcString += cardString(dc.name, dc.image); }
+    dcString += "</div>";
+    dcEasyString += dcString;
+  }
+
+  for (let i = 0; i < mediumChallenges.length; i += 2) {
+    const dcs = [mediumChallenges[i], mediumChallenges[i + 1]]
+    let dcString = '<div class="row">';
+    for (const dc of dcs) { dcString += cardString(dc.name, dc.image); }
+    dcString += "</div>";
+    dcMediumString += dcString;
+  }
+
+  for (let i = 0; i < hardChallenges.length; i += 2) {
+    const dcs = [hardChallenges[i], hardChallenges[i + 1]]
+    let dcString = '<div class="row">';
+    for (const dc of dcs) { dcString += cardString(dc.name, dc.image); }
+    dcString += "</div>";
+    dcHardString += dcString;
+  }
+
+  dcEasyContainer.innerHTML += dcEasyString;
+  dcMediumContainer.innerHTML += dcMediumString;
+  dcHardContainer.innerHTML += dcHardString;
+}
+
+window.addEventListener("load", init);
